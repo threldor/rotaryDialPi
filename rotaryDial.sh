7301689a62f422c91f5b224d6d8d43a6d4bffc81
threldor <threldor@gmail.com>
2012-11-11 22:12:25 +0200

    initial rotaryDial
    only a few functions written

--- rotaryDial.sh
+++ rotaryDial.sh
@@ -1,24 +1,69 @@
 #!/bin/bash
-t1="/sys/bus/w1/devices/28-000003eb83e3/w1_slave"
-t2="/sys/bus/w1/devices/28-000003c1bf9e/w1_slave"
 
-function read_temp() {
-  res=`cat "$1"` 
-  echo $res | grep -s  "YES" >/dev/null
-  if [[ $? -eq 0 ]] ; then
-    t=`echo $res| head -1 | awk 'BEGIN{FS="="} { print ($3/1000) }'`
-  else
-    t="BAD"
-  fi
-  echo $t
+# rotaryDial:
+#       Read a rotary dial phone on the pi
+#
+#       Wait for the handset to be lifted then monitor the ready pin
+#       should this change we need then count the number of pulses 
+#       that occur before the ready pin returns to its original state.
+#       If the handset is hung up we abort and continue to monitor it.
+#
+#       James Connor, November 2012
+
+# input pin numbers
+readyPin=0
+pulsePin=1
+handsetPin=2
+
+# setup:
+#       Initialise the GPIO
+#######################################################################
+
+setup()
+{
+  echo Setup
+  for i in $readyPin $pulsePin $handsetPin ; do gpio mode $i in ; done
 }
 
-while true; do
-  temperature=`read_temp "$t1"`
-  d=`date +%s`
-  if [ $temperature != "BAD" ];then
-    break
-  fi
-done
+# waitHandset:
+#       Wait for the handset to be lifted. Because we have the GPIO
+#       pin pulled high, we wait for it to go low.
+#######################################################################
 
-echo "$temperature" 
+waitHandset ()
+{
+  echo -n "Waiting for handset ... "
+  while [ `gpio read $handsetPin` = 1 ]; do
+    sleep 0.2
+  done
+  echo "Handset lifted"
+}
+
+# waitReady:
+#       Wait for the ready pin to change state indicating the phone is 
+#       being dialed. We also need to monitor the handset to see if
+#       this is hung up. 
+#######################################################################
+
+waitReady ()
+{
+  echo -n "Waiting for ready ... "
+  while [ `gpio read $readyPin` = 1 ]; do
+    if [ `gpio read $handsetPin` = 1 ] ; then
+      # handset hung up
+      break
+    fi
+    sleep 0.1
+  done
+  echo "Handset lifted"
+}
+
+#######################################################################
+# The main program
+#       Setup pins then monitor the handsetPin and go from there.
+#######################################################################
+
+setup
+while true; do
+  waitHandset
+done
