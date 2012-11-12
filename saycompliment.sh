1a594bb1132f976e8bef422ff1dc2019e14dc02f
threldor <threldor@gmail.com>
2012-11-12 21:53:02 +0200

    say compliment

--- saycompliment.sh
+++ saycompliment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 compliment=`sort -R compliments.txt | head -n 1`
 function say() { 
-  local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$compliment"; 
+  local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q= $compliment "; 
 }
 say
