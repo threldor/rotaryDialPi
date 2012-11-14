5fe88b18a29681991c2c66d368d6fe866ec97aa9
threldor <threldor@gmail.com>
2012-11-14 19:22:28 +0200

    updated wunder and weather

--- wunder.sh
+++ wunder.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # wunder.sh
-curl --silent "http://api.wunderground.com/api/MYKEY/conditions/q/Estonia/Tallinn.json" | awk -F ':'  '
+key=$(cat wunderkey)
+curl --silent "http://api.wunderground.com/api/$key/conditions/q/Estonia/Tallinn.json" | awk -F ':'  '
 /city/ { sub(",", "", $2) && c=$2 }
-/temp_c/ { sub(",", "", $2); printf("The current temperature in %s today is %s degrees", c, $2); exit}'
+/temp_c/ { sub(",", "", $2); printf("The current temperature in %s today is %s degrees\n", c, $2); exit}'
