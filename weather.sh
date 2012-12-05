54c01310a67849e9decba798b2f7342a2074052b
threldor <threldor@gmail.com>
2012-12-05 20:58:11 +0200

    gave weather arguments

--- weather.sh
+++ weather.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 # weather.sh
-curl --silent "http://weather.yahooapis.com/forecastrss?w=845743&u=c" | awk -F '- '  '
-/<title>/ { gsub(",.*", "", $2) && l=$2 }
-/<b>Current Conditions:/ { getline; gsub("<.*", "", $1) && m=$1 }
-/<b>Forecast/ { getline; getline; gsub("<.*", "", $2); printf("The weather for %s:\nCurrently is %s\nTomorrow is %s\n", l, m, $2); exit }'
+
+if [ "$1" = "-today" ] ; then
+  curl --silent "http://weather.yahooapis.com/forecastrss?w=845743&u=c" | awk -F '- '  '
+  /<title>/ { gsub(",.*", "", $2) && l=$2 }
+  /<b>Current Conditions:/ { getline; gsub("<.*", "", $1) && m=$1 }
+  /<b>Forecast/ { getline; gsub("<.*", "", $2); printf("The weather for %s:\nCurrently is %s\nTomorrow is %s\n", l, m, $2); exit }'
+elif [ "$1" = "-tomorrow" ] ; then
+  curl --silent "http://weather.yahooapis.com/forecastrss?w=845743&u=c" | awk -F '- '  '
+  /<title>/ { gsub(",.*", "", $2) && l=$2 }
+  /<b>Current Conditions:/ { getline; gsub("<.*", "", $1) && m=$1 }
+  /<b>Forecast/ { getline; getline; gsub("<.*", "", $2); printf("The weather for %s:\nCurrently is %s\nTomorrow is %s\n", l, m, $2); exit }'
+fi
+
