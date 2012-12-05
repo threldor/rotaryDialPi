e4ac92ee134cf33461088109d733e348b34d78be
threldor <threldor@gmail.com>
2012-12-05 19:53:11 +0200

    Revert "correct forecast for current conditions"
    This reverts commit 501edfc660bcbead8415c036c8d2c26ba445a991.

--- weather.sh
+++ weather.sh
@@ -3,5 +3,4 @@
 curl --silent "http://weather.yahooapis.com/forecastrss?w=845743&u=c" | awk -F '- '  '
 /<title>/ { gsub(",.*", "", $2) && l=$2 }
 /<b>Current Conditions:/ { getline; gsub("<.*", "", $1) && m=$1 }
-/<b>Forecast/ { getline;; gsub("<.*", "", $2) && n=$2 }
-/<b>Forecast/ { getline; gsub("<.*", "", $2); printf("The weather for %s:\nCurrently it is %s\nToday is %s\nTomorrow is %s\n", l, m, n, $2); exit }'
+/<b>Forecast/ { getline; gsub("<.*", "", $2); printf("The weather for %s:\nToday is %s\nTomorrow is %s\n", l, m, $2); exit }'
