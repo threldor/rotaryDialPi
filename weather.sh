953f169c732df8ca55033e3f4121184df59dfe76
threldor <threldor@gmail.com>
2012-12-05 20:33:24 +0200

    fixed forecast

--- weather.sh
+++ weather.sh
@@ -3,4 +3,4 @@
 curl --silent "http://weather.yahooapis.com/forecastrss?w=845743&u=c" | awk -F '- '  '
 /<title>/ { gsub(",.*", "", $2) && l=$2 }
 /<b>Current Conditions:/ { getline; gsub("<.*", "", $1) && m=$1 }
-/<b>Forecast/ { getline; gsub("<.*", "", $2); printf("The weather for %s:\nToday is %s\nTomorrow is %s\n", l, m, $2); exit }'
+/<b>Forecast/ { getline; getline; gsub("<.*", "", $2); printf("The weather for %s:\nCurrently is %s\nTomorrow is %s\n", l, m, $2); exit }'
