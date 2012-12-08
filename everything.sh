f9ef676d8bd991a3064755c44bcc605369b6ddc7
threldor <threldor@gmail.com>
2012-12-08 10:35:13 +0200

    working with only weather and comp

--- everything.sh
+++ everything.sh
@@ -43,7 +43,8 @@ setup()
 #       say the passed arguments
 #######################################################################
 
-say() { 
+say()
+{ 
   local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?tl=en&q=$*"; 
 }
 
